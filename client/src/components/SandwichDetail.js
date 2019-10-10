8af64488534aa12ae8c45e5a16f46a6e3a773ac8
Barrett Clark <barrettclark@Barretts-MacBook-Pro.local>
2019-10-10 11:58:14 -0400

    added complete sandwich detail page

--- client/src/components/SandwichDetail.js
+++ client/src/components/SandwichDetail.js
@@ -6,11 +6,29 @@ class SandwichDetail extends Component {
         sandwich: {}
     };
 
+    getSingleSandwich = () => {
+        fetch(`/api/sandwich/${this.props.match.params.id}/`)
+        .then(res => res.json())
+        .then(sandwich => {
+            this.setState({ sandwich })
+        })
+    }
+
+    componentDidMount = () => {
+        this.getSingleSandwich();
+    }
+
     render() {
         return (
             <div>
                 <Link to="/">Home</Link>
-                <h1>Hello World</h1>
+                <h1>{this.state.sandwich.name}</h1>
+                <ul>
+                    <li>{this.state.sandwich.location}</li>
+                    <li>{this.state.sandwich.description}</li>
+                    <li>{this.state.sandwich.createdOn}</li>
+                    
+                </ul>
             </div>
         )
     }
